cd63c3ed43ab07ba52b783ab38fde304abbf7dca
binhboong2012 <binhcauduongbo@gmail.com>
2022-03-15 15:46:44 +0700

    Bài thực hành kết thúc module3

---
+++ product_management.sql
@@ -0,0 +1,30 @@
+create database product_management;
+
+use product_management;
+
+create table category(
+     id int primary key auto_increment,
+     name varchar(50)
+);
+
+create table product(
+    id int primary key auto_increment,
+    name varchar(50),
+    price double,
+    quantity int,
+    color varchar(50),
+    description varchar(50),
+    categoryId int,
+    foreign key (categoryId) references category(id)
+);
+
+
+insert into product(name, price,quantity, color, description,categoryId)
+values('iphone',13000,1,'red','new',1),
+       ('samsung',15000,1,'white','new',2);
+
+
+
+insert into category(name)
+values('smartphone'),
+       ('tablet');
---
+++ src/main/java/com/codegym/dao/productDao/ProductDao.java
@@ -0,0 +1,152 @@
+package com.codegym.dao.productDao;
+
+import com.codegym.dao.DBConnection;
+import com.codegym.model.Product;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductDao implements IGeneralProductDao {
+    public static final String JDBC_SELECT_ALL = "select * from product join category on product.categoryId = category.id";
+    public static final String JDBC_SELECT_BY_ID = "select * from product join category on product.categoryId = category.id where product.id = ?";
+    public static final String JDBC_INSERT_PRODUCT = "insert into product(name, price, quantity,color, description, categoryId) values(?,?,?,?,?,?)";
+    public static final String JDBC_UPDATE_PRODUCT = "update product set name = ?, price = ?,quantity=? ,color =?, description = ?, categoryId = ? where id = ?";
+    public static final String JDBC_DELETE_PRODUCT_BY_ID = "delete from product where id = ?";
+    public static final String JDBC_FIND_PRODUCT_NAME = "select * from product join category on product.categoryId = category.id where product.name like ?";
+    private Connection connection;
+
+    {
+        try {
+            connection = DBConnection.getConnection();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ProductDao() {
+    }
+
+    @Override
+    public List<Product> findAll() throws SQLException, ClassNotFoundException {
+        List<Product> productList = new ArrayList<>();
+        Product product;
+
+        PreparedStatement preparedStatement = connection.prepareStatement(JDBC_SELECT_ALL);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        while (resultSet.next()){
+            int id = resultSet.getInt(1);
+            String name = resultSet.getString(2);
+            Double price = resultSet.getDouble(3);
+            int quantity = resultSet.getInt(4);
+            String color = resultSet.getString(5);
+            String description = resultSet.getString(6);
+            String category = resultSet.getString(9);
+            product = new Product(id,name, price,quantity,color,description,category);
+            productList.add(product);
+        }
+        return productList ;
+    }
+
+    @Override
+    public Product findById(int id) throws SQLException, ClassNotFoundException {
+        Product product;
+
+        PreparedStatement preparedStatement = connection.prepareStatement(JDBC_SELECT_BY_ID);
+        preparedStatement.setInt(1,id);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        while(resultSet.next()) {
+            int id1 = resultSet.getInt(1);
+            String name = resultSet.getString(2);
+            Double price = resultSet.getDouble(3);
+            int quantity = resultSet.getInt(4);
+            String color = resultSet.getString(5);
+            String description = resultSet.getString(6);
+            int categoryId = resultSet.getInt(8);
+            String category = resultSet.getString(9);
+            product = new Product(id1,name, price,quantity, color, description, category,categoryId);
+            return product;
+        }
+        return null;
+    }
+
+    @Override
+    public boolean createProduct(Product product) throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement(JDBC_INSERT_PRODUCT);
+        preparedStatement.setString(1, product.getName());
+        preparedStatement.setDouble(2, product.getPrice());
+        preparedStatement.setInt(3,product.getQuantity());
+        preparedStatement.setString(4, product.getColor());
+        preparedStatement.setString(5, product.getDescription());
+        preparedStatement.setInt(6, product.getCategoryId());
+
+        return preparedStatement.executeUpdate()>0;
+    }
+
+    @Override
+    public boolean updateProductById(int id, Product product) {
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(JDBC_UPDATE_PRODUCT);
+            preparedStatement.setString(1,product.getName());
+            preparedStatement.setDouble(2,product.getPrice());
+            preparedStatement.setInt(3,product.getQuantity());
+            preparedStatement.setString(4,product.getColor());
+            preparedStatement.setString(5,product.getDescription());
+            preparedStatement.setInt(6,product.getCategoryId());
+            preparedStatement.setInt(7,id);
+            return  preparedStatement.executeUpdate()>0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean deleteProductById(int id) {
+
+        PreparedStatement preparedStatement = null;
+        try {
+            preparedStatement = connection.prepareStatement(JDBC_DELETE_PRODUCT_BY_ID);
+            preparedStatement.setInt(1,id);
+            return preparedStatement.executeUpdate()>0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
+    @Override
+    public List<Product> searchProductName(String searchName) {
+        try {
+            Product product;
+            List<Product> products = new ArrayList<>();
+            PreparedStatement preparedStatement = connection.prepareStatement(JDBC_FIND_PRODUCT_NAME);
+            preparedStatement.setString(1, searchName);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while(resultSet.next()) {
+                int id1 = resultSet.getInt(1);
+                String name = resultSet.getString(2);
+                Double price = resultSet.getDouble(3);
+                int quantity = resultSet.getInt(4);
+                String color = resultSet.getString(5);
+                String description = resultSet.getString(6);
+                int categoryId = resultSet.getInt(8);
+                String category = resultSet.getString(9);
+                product = new Product(id1,name, price,quantity, color, description, category,categoryId);
+                products.add(product);
+
+            }
+            return products;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
